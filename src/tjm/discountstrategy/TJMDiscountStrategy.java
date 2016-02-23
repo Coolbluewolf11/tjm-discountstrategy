b2f24a53a1872c9340f50d95d30930cf16417204
Tim <Tim@10.30.19.228>
2016-02-23 15:09:54 -0600

    first commit

---
+++ src/tjm/discountstrategy/TJMDiscountStrategy.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tjm.discountstrategy;
+
+/**
+ *
+ * @author Tim
+ */
+public class TJMDiscountStrategy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // KLUDGE: do configuration here
+        
+        //Start talking to objects
+    }
+    
+}
